361c4e26d0dae862dd2f948f3fe8466f7ccfb1f8
VyDucTan <vdt5snet@gmail.com>
2023-10-13 21:52:45 +0700

    feat(icons): use iconify/react

--- icons/index.tsx
+++ icons/index.tsx
@@ -5,3 +5,11 @@ export { default as CloseOutlined } from "./CloseOutlined";
 export * from "./LogoutOutlined";
 export * from "./UserOutlined";
 export { default as WarningFilled } from "./WarningFilled";
+
+import { Icon as Iconify, IconProps as IconifyProps } from "@iconify/react";
+import { clsm } from "@vyductan/utils";
+
+type IconProps = IconifyProps;
+export const Icon = ({ className, ...props }: IconProps) => {
+  return <Iconify className={clsm("w-5 h-5", className)} {...props} />;
+};
