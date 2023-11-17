4e96146658a2a702dacba2798bbe017044121ded
VyDucTan <vdt5snet@gmail.com>
2023-11-17 16:20:12 +0700

    feat(icons): add prop srOnly

--- icons/Icon.tsx
+++ icons/Icon.tsx
@@ -4,7 +4,14 @@ import type { IconProps as IconifyProps } from "@iconify/react";
 import { clsm } from "@vyductan/utils";
 
 // https://icon-sets.iconify.design/
-export type IconProps = IconifyProps;
-export const Icon = ({ className, ...props }: IconProps) => {
-  return <Iconify className={clsm("h-5 w-5", className)} {...props} />;
+export type IconProps = IconifyProps & {
+  srOnly?: string;
+};
+export const Icon = ({ className, srOnly, ...props }: IconProps) => {
+  return (
+    <>
+      <Iconify className={clsm("h-5 w-5", className)} {...props} />
+      {srOnly && <span className="sr-only">{srOnly}</span>}
+    </>
+  );
 };
