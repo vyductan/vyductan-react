b9858771d185530c6dff9cb9b51ae56918421ab1
VyDucTan <vdt5snet@gmail.com>
2023-12-28 13:35:34 +0700

    feat(components-pro): add components-pro package with page-container and page-header components feat(components-pro): add package.json, tailwind.config.ts, and tsconfig.json files for components-pro package

---
+++ components-pro/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+import baseConfig from "@vyductan/theme-config";
+
+export default {
+  content: ["./**/*.{ts,tsx}"],
+  presets: [baseConfig],
+} satisfies Config;
