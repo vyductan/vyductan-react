c42707948dd92d7114acea2578b8ae47d855873d
VyDucTan <vdt5snet@gmail.com>
2024-01-12 13:18:54 +0700

    feat(date-picker): add DatePicker component
    - Added a new file `DatePicker.tsx` in the `ui/src/date-picker` directory.
    - The `DatePicker` component is a reusable component that provides a date picker functionality.
    - The component supports two modes: "single" and "range".
    - In "single" mode, the component displays a single selected date or a placeholder if no date is selected.
    - In "range" mode, the component displays a range of selected dates or a placeholder if no range is selected.
    - The component uses the `react-day-picker` library for the date picker functionality.
    - The component also uses other components such as `Button`, `Calendar`, and `Popover` from the UI library.
    - The component is exported from `ui/src/date-picker/index.ts`.

---
+++ ui/src/date-picker/DatePicker.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import type { SelectSingleEventHandler } from "react-day-picker";
+import * as React from "react";
+import { format } from "date-fns";
+
+import { Icon } from "@vyductan/icons";
+import { clsm } from "@vyductan/utils";
+
+import { Button } from "../button";
+import { Calendar } from "../calendar";
+import { Popover } from "../popover";
+
+type DateRange = {
+  start: Date | undefined;
+  end?: Date | undefined;
+};
+
+export type DatePickerSingleProps = {
+  mode: "single";
+  value?: Date;
+  onChange?: SelectSingleEventHandler;
+};
+export type DatePickerRangeProps = {
+  mode: "range";
+  value?: DateRange;
+  onChange?: (range: DateRange | undefined) => void;
+};
+export type DatePickerProps = DatePickerSingleProps | DatePickerRangeProps;
+const DatePickerInternal = (
+  props: DatePickerProps,
+  ref: React.Ref<HTMLButtonElement>,
+) => {
+  const valueToDisplay =
+    props.mode === "range" ? (
+      props.value?.start ? (
+        props.value.end ? (
+          <>
+            {format(props.value.start, "LLL dd, y")} -{" "}
+            {format(props.value.end, "LLL dd, y")}
+          </>
+        ) : (
+          format(props.value.start, "LLL dd, y")
+        )
+      ) : (
+        <span className="text-placeholder">From Date - To Date</span>
+      )
+    ) : props.mode === "single" ? (
+      props.value ? (
+        format(props.value, "PPP")
+      ) : (
+        <span className="text-placeholder">Pick a date</span>
+      )
+    ) : null;
+  const picker = (() => {
+    if (props.mode === "single") {
+      const { value, onChange, ...rest } = props;
+      return (
+        <Calendar initialFocus selected={value} onSelect={onChange} {...rest} />
+      );
+    }
+    if (props.mode === "range") {
+      const { value, onChange, ...rest } = props;
+      return (
+        <Calendar
+          initialFocus
+          selected={{
+            from: value?.start,
+            to: value?.end,
+          }}
+          onSelect={(range) => {
+            onChange?.({
+              start: range?.from,
+              end: range?.to,
+            });
+          }}
+          numberOfMonths={2}
+          {...rest}
+          mode="range"
+        />
+      );
+    }
+    return null;
+  })();
+
+  return (
+    <Popover className="w-auto p-0" content={picker}>
+      <Button
+        className={clsm(
+          "flex w-full",
+          "justify-start text-left font-normal",
+          !props.value && "text-muted-foreground",
+        )}
+        ref={ref}
+      >
+        {valueToDisplay}
+        <Icon
+          icon="mingcute:calendar-2-line"
+          className="ml-auto h-4 w-4 opacity-50"
+        />
+      </Button>
+    </Popover>
+  );
+};
+
+export const DatePicker = React.forwardRef(DatePickerInternal);
