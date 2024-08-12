d8fdad386522ff86b94c84c383344f2ccde334e6
vyductan <vdt5snet@gmail.com>
2024-08-12 09:44:28 +0700

    refactor(ui): Remove unused nextjsConfig import and spread operator in eslint config.

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,5 +1,4 @@
 import baseConfig from "@acme/eslint-config/base";
-import nextjsConfig from "@acme/eslint-config/nextjs";
 import reactConfig from "@acme/eslint-config/react";
 import tailwindConfig from "@acme/eslint-config/tailwind";
 
@@ -10,6 +9,5 @@ export default [
   },
   ...baseConfig,
   ...reactConfig,
-  ...nextjsConfig,
-  ...tailwindConfig
+  ...tailwindConfig,
 ];
