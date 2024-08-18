a3b5bf1d13fd1abb01a567a65291baab8a9e2b19
vyductan <vdt5snet@gmail.com>
2024-08-18 19:25:33 +0700

    refactor(eslint): Update parser options and plugins in base.js and tailwind.js
    Updated parser options in base.js and tailwind.js for better TypeScript support.
    Also updated eslint-plugin-turbo and typescript-eslint versions in package.json.

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -2,8 +2,8 @@
 // https://github.com/t3-oss/create-t3-turbo/blob/main/tooling/eslint/base.js
 
 /// <reference types="./types.d.ts" />
-// import * as path from "node:path";
-// import { includeIgnoreFile } from "@eslint/compat";
+import * as path from "node:path";
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import turboPlugin from "eslint-plugin-turbo";
@@ -12,34 +12,36 @@ import tseslint from "typescript-eslint";
 /**
  * All packages that leverage t3-env should use this rule
  */
-export const restrictEnvAccess = tseslint.config({
-  files: ["**/*.js", "**/*.ts", "**/*.tsx"],
-  rules: {
-    "no-restricted-properties": [
-      "error",
-      {
-        object: "process",
-        property: "env",
-        message:
-          "Use `import { env } from '~/env'` instead to ensure validated types.",
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        name: "process",
-        importNames: ["env"],
-        message:
-          "Use `import { env } from '~/env'` instead to ensure validated types.",
-      },
-    ],
+export const restrictEnvAccess = tseslint.config(
+  { ignores: ["**/env.ts"] },
+  {
+    rules: {
+      "no-restricted-properties": [
+        "error",
+        {
+          object: "process",
+          property: "env",
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "process",
+          importNames: ["env"],
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+    },
   },
-});
+);
 
 export default tseslint.config(
   // Ignore files not tracked by VCS and any config files
-  // includeIgnoreFile(path.join(import.meta.dirname, "../../.gitignore")),
-  { ignores: ["**/*.config.*", "**/*.d.ts"] },
+  includeIgnoreFile(path.join(import.meta.dirname, "../../../.gitignore")),
+  // { ignores: ["**/*.config.*", "**/*.d.ts"] },
   {
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     plugins: {
@@ -104,6 +106,11 @@ export default tseslint.config(
   },
   {
     linterOptions: { reportUnusedDisableDirectives: true },
-    languageOptions: { parserOptions: { projectService: true } },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        // tsconfigRootDir: import.meta.dirname,
+      },
+    },
   },
 );
--- tooling/eslint/tailwind.js
+++ tooling/eslint/tailwind.js
@@ -1,24 +1,28 @@
+import * as path from "node:path";
 import { fileURLToPath } from "url";
+// @ts-expect-error
 import tailwindPlugin from "eslint-plugin-tailwindcss";
-import tailwind from "tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
+const config = path.join(import.meta.dirname, "../tailwind/web.ts");
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default [
+  ...tailwindPlugin.configs["flat/recommended"],
   {
-    plugins: {
-      tailwindcss: tailwindPlugin,
-    },
+    // plugins: {
+    //   tailwindcss: tailwindPlugin,
+    // },
     // rules: {
     //   ...tailwindPlugin.rules,
     // },
     settings: {
       tailwindcss: {
         callees: ["clsm", "cva", "cx"],
-        config: fileURLToPath(
-          new URL("../tailwind/web.ts", "file://" + __filename),
-        ),
+        config,
+        // config: fileURLToPath(
+        //   new URL("../tailwind/web.ts", "file://" + __filename),
+        // ),
         cssFiles: [
           "**/*.{css,scss}",
           "!**/node_modules",
