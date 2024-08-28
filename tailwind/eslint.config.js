bd1317c72ea1e25210d9217116bf1f6bb1e12a6f
Vy Duc Tan <vdt5snet@gmail.com>
2024-08-28 10:09:02 +0700

    nextjs (#42)
    * chore: Update package versions in pnpm-lock.yaml
    
    * refactor: Update import paths to use "@acme/db"
    
    This commit updates the import paths in the API to use "@acme/db" for 
    consistency and clarity. The changes were made to improve the structure 
    and organization of the codebase.
    
    * refactor(nextjs): Remove unused code in Tanstack Route
    
    Removed unused code related to route definition and type declarations in
    order to clean up the codebase and improve maintainability.
    
    * refactor: Use InputSearch component in ProjectDetailPage
    
     feat: Create WordDetailPage component
     chore: Update import path for auth module
     chore: Update import path for Session in UserNav
     chore: Update import path for signIn in signin page
     refactor: Remove unnecessary catch parameter
     chore: Update version of @tanstack/react-router and @tanstack/router-devtools
     chore: Add NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX to env configuration
     refactor: Remove unused PromptForm component
    
    * refactor: Update GitHub setup path in CI workflow and remove unused comments and reference types

--- tailwind/eslint.config.js
+++ tailwind/eslint.config.js
@@ -1,6 +1,4 @@
-// FIXME: This kinda stinks...
-/// <reference types="../../tooling/eslint/types.d.ts" />
-
 import baseConfig from "@acme/eslint-config/base";
 
+/** @type {import('typescript-eslint').Config} */
 export default [...baseConfig];
