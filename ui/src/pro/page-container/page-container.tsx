3fc1e1193d1ce281a2d6721e0faed4df64a12e2a
vyductan <vdt5snet@gmail.com>
2024-10-06 10:37:01 +0700

    feat(ui/page-container): Add loading state to PageContainer component

--- ui/src/pro/page-container/page-container.tsx
+++ ui/src/pro/page-container/page-container.tsx
@@ -7,19 +7,35 @@ import { PageHeader } from "../page-header";
 export type PageContainerProps = {
   children: ReactNode;
   header?: PageHeaderProps;
+  className?: string;
   content?: {
     className?: string;
   };
+  loading?: boolean;
 };
 export const PageContainer = ({
   children,
   header,
+  className,
   content,
+  loading = false,
 }: PageContainerProps) => {
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-3 lg:p-6">
-      {header && <PageHeader {...header} />}
-      <div className={clsm("relative", content?.className)}>{children}</div>
+    <main
+      className={clsm(
+        "mx-auto w-full max-w-screen-lg",
+        "flex flex-1 flex-col gap-4 p-4 lg:gap-3 lg:p-6",
+        className,
+      )}
+    >
+      {loading ? (
+        <>Loading</>
+      ) : (
+        <>
+          {header && <PageHeader {...header} />}
+          <div className={clsm("relative", content?.className)}>{children}</div>
+        </>
+      )}
     </main>
   );
 };
