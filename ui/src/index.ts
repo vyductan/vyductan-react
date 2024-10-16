4442ce3e75209c6083b91396adf8b38426bb797a
vyductan <vdt5snet@gmail.com>
2024-10-16 11:27:37 +0700

    refactor(ui/clsm): Update tailwind-merge function to extend and customize class Groups

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,8 +1,15 @@
 import { cx } from "class-variance-authority";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
 
 export * from "./types";
 
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      w: ["w-screen-sm", "w-screen-md", "w-screen-xl"],
+    },
+  },
+});
 const clsm = (...inputs: Parameters<typeof cx>) => twMerge(cx(inputs));
 
 export { clsm };
