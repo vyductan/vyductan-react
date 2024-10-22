e9348d7ae89a0bc8e73c848fb05e383a905c10db
vyductan <vdt5snet@gmail.com>
2024-10-22 09:26:26 +0700

    feat: change "clsm" to use tailwind-variants

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,15 +1,27 @@
-import { cx } from "class-variance-authority";
-import { extendTailwindMerge } from "tailwind-merge";
+import { defaultConfig } from "tailwind-variants";
 
-export * from "./types";
-
-const twMerge = extendTailwindMerge({
+defaultConfig.twMergeConfig = {
   extend: {
     classGroups: {
       w: ["w-screen-sm", "w-screen-md", "w-screen-xl"],
     },
   },
-});
-const clsm = (...inputs: Parameters<typeof cx>) => twMerge(cx(inputs));
+};
 
-export { clsm };
+// import { cx } from "class-variance-authority";
+// import { extendTailwindMerge } from "tailwind-merge";
+//
+export * from "./types";
+//
+// const twMerge = extendTailwindMerge({
+//   extend: {
+//     classGroups: {
+//       w: ["w-screen-sm", "w-screen-md", "w-screen-xl"],
+//     },
+//   },
+// });
+// const clsm = (...inputs: Parameters<typeof cx>) => twMerge(cx(inputs));
+//
+// export { clsm };
+
+export { cnBase as clsm } from "tailwind-variants";
