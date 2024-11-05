6b97363e033770cb1494a762493d32c7dbb969c0
vyductan <vdt5snet@gmail.com>
2024-11-05 18:33:36 +0700

    feat(sidebar): new sidebar component

--- tailwind/base.ts
+++ tailwind/base.ts
@@ -211,6 +211,17 @@ export default {
           active: baseColors.gray[600],
         },
         placeholder: baseColors.gray[700],
+
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       /* End Colors */
 
