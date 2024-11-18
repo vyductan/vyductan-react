284c21d0fc4371ad7572fd9e984227a4fc1d6e6a
vyductan <vdt5snet@gmail.com>
2024-11-18 14:18:16 +0700

    feat(radio): color

---
+++ ui/src/radio/colors.ts
@@ -0,0 +1,23 @@
+export const radioColors: Record<string, string> = {
+  default: "[&>button]:border-primary [&>button]:text-primary",
+  success:
+    "text-green-900 [&>button]:border-green-600 [&>button]:text-green-600",
+  processing:
+    "text-blue-900 [&>button]:border-blue-600 [&>button]:text-blue-600",
+  error: "text-red-900 [&>button]:border-red-600 [&>button]:text-red-600",
+  warning:
+    "text-amber-900 [&>button]:border-amber-600 [&>button]:text-amber-600",
+
+  gray: "text-gray-900 [&>button]:border-gray-600 [&>button]:text-gray-600",
+  amber: "text-amber-900 [&>button]:border-amber-600 [&>button]:text-amber-600",
+  blue: "text-blue-900 [&>button]:border-blue-600 [&>button]:text-blue-600",
+  fuchsia:
+    "text-fuchsia-900 [&>button]:border-fuchsia-600 [&>button]:text-fuchsia-600",
+  green: "text-green-900 [&>button]:border-green-600 [&>button]:text-green-600",
+  red: "text-red-900 [&>button]:border-red-600 [&>button]:text-red-600",
+  rose: "text-rose-900 [&>button]:border-rose-600 [&>button]:text-rose-600",
+  pink: "text-pink-900 [&>button]:border-pink-600 [&>button]:text-pink-600",
+  purple:
+    "text-purple-900 [&>button]:border-purple-600 [&>button]:text-purple-600",
+  teal: "text-teal-900 [&>button]:border-teal-600 [&>button]:text-teal-600",
+};
--- ui/src/radio/radio.tsx
+++ ui/src/radio/radio.tsx
@@ -4,6 +4,7 @@ import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import type { ValueType } from "../form";
 import { cn } from "..";
 import { Icon } from "../icons";
+import { radioColors } from "./colors";
 
 type RadioProps = Omit<
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>,
@@ -11,20 +12,33 @@ type RadioProps = Omit<
 > & {
   label?: React.ReactNode;
   value?: ValueType;
+  color?: string;
 };
 const Radio = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   RadioProps
->(({ label, className, value, ...props }, ref) => {
+>(({ value, label, disabled, className, color = "default", ...props }, ref) => {
   return (
-    <label className={cn("flex items-center", "text-sm", className)}>
+    <label
+      className={cn(
+        "flex items-center",
+        "cursor-pointer text-sm",
+        radioColors[color],
+        disabled &&
+          "cursor-not-allowed text-foreground opacity-30 [&>button]:border-foreground",
+        className,
+      )}
+    >
       <RadioGroupPrimitive.Item
         ref={ref}
         value={value as string}
         className={cn(
-          "aspect-square size-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "aspect-square size-4 rounded-full border shadow ring-offset-background",
+          "focus:outline-none focus-visible:ring-1 focus-visible:ring-ring",
+          "disabled:cursor-not-allowed disabled:opacity-50",
           "data-[state=checked]:border-2",
         )}
+        disabled={disabled}
         {...props}
       >
         <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
@@ -34,7 +48,7 @@ const Radio = React.forwardRef<
           />
         </RadioGroupPrimitive.Indicator>
       </RadioGroupPrimitive.Item>
-      <span className="cursor-pointer px-2">{label}</span>
+      <span className="px-2">{label}</span>
     </label>
   );
 });
