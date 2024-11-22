2a12bf222d44ba5e723cf9cb0e41c05250c07542
vyductan <vdt5snet@gmail.com>
2024-11-22 16:53:37 +0700

    fix(tag): rounded-full to rounded-md

--- ui/src/tag/tag.tsx
+++ ui/src/tag/tag.tsx
@@ -1,5 +1,4 @@
 import type { VariantProps } from "class-variance-authority";
-import * as React from "react";
 import { tv } from "tailwind-variants";
 
 import { cn } from "..";
@@ -24,12 +23,13 @@ const color: Record<string, string> = {
 };
 const tagVariants = tv({
   base: [
-    "text-xs",
-    "inline-flex items-center justify-center rounded-full border px-2.5 py-0.5 font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    "inline-flex items-center justify-center rounded-md border px-2.5 py-0.5 text-xs font-medium transition-colors",
+    "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+    "h-[22px] whitespace-nowrap",
   ],
   variants: {
     variant: {
-      default: ["border-transparent", "whitespace-nowrap"],
+      default: "border-transparent",
       secondary:
         "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
       destructive:
