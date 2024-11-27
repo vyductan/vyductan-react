542cc8638b4ea41e99b4b207c623bad270ce5a41
vyductan <vdt5snet@gmail.com>
2024-11-27 16:05:42 +0700

    fix(ui-button): improve link button styles

--- ui/src/button/button.tsx
+++ ui/src/button/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = tv({
   ],
   variants: {
     primary: {
-      true: ["border-primary", "hover:border-primary-hover "],
+      true: ["border-primary", "hover:border-primary-hover"],
     },
     action: {
       true: [],
@@ -38,6 +38,12 @@ const buttonVariants = tv({
       gray: [],
       teal: [],
       pink: [],
+    },
+    size: {
+      sm: "h-6 rounded-sm px-2 py-0 font-normal",
+      default: "rounded-md px-3 py-1",
+      lg: "h-10 rounded-lg px-4 py-2",
+      xl: "h-12 rounded-lg px-4 py-2.5 text-lg",
     },
     variant: {
       default: [
@@ -59,13 +65,8 @@ const buttonVariants = tv({
         "data-[state=open]:bg-background-hover",
       ],
       light: ["border-transparent", "hover:bg-background-hover"],
-      link: "border-transparent !px-0 underline-offset-4 hover:underline",
-    },
-    size: {
-      sm: "h-6 rounded-sm px-2 py-0 font-normal",
-      default: "h-8 rounded-md px-3 py-1",
-      lg: "h-10 rounded-lg px-4 py-2",
-      xl: "h-12 rounded-lg px-4 py-2",
+      link: "border-0 p-0 font-normal underline-offset-2 hover:underline",
+      text: "border-0 px-0",
     },
     shape: {
       default: "",
@@ -236,7 +237,7 @@ const buttonVariants = tv({
     {
       size: "default",
       shape: ["icon", "circle"],
-      className: "w-8",
+      className: "size-8",
     },
     {
       size: "lg",
@@ -307,13 +308,16 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const buttonConfig = useUi((state) => state.componentConfig?.button);
 
     const Comp = asChild || href ? Slot : "button";
-
+    console.log("ssssss", size);
     const ChildrenToRender = (
       <>
         {(!!loading || icon) && (
           <GenericSlot<Partial<IconProps>>
             className={cn(
-              size === "sm" ? "" : "size-4",
+              "size-4",
+              size === "sm" && "size-4",
+              size === "lg" && "size-4",
+              size === "xl" && "size-[18px]",
               // children ? "mr-2" : "",
             )}
             // srOnly={
