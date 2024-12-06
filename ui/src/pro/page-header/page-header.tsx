b34c88fc3aa7b4ed10a4499083bd531e0741bcfd
vyductan <vdt5snet@gmail.com>
2024-12-06 18:21:03 +0700

    feat(page-header): render breadcrumb

--- ui/src/pro/page-header/page-header.tsx
+++ ui/src/pro/page-header/page-header.tsx
@@ -1,20 +1,34 @@
 // import { ArrowLeftOutlined, ArrowRightOutlined } from "@acme/ui/icons";
 
 import type { AvatarProps } from "../../avatar";
+import type { BreadcrumbProps } from "../../breadcrumb";
 import type { TagProps } from "../../tag";
 import { cn } from "../..";
 import { Avatar } from "../../avatar";
+import { Breadcrumb } from "../../breadcrumb";
 
 type PageHeaderProps = {
   title?: React.ReactNode;
   subTitle?: React.ReactNode;
   children?: React.ReactNode;
-  className?: string;
-  backIcon?: React.ReactNode;
   extra?: React.ReactNode;
+  breadcrumb?: BreadcrumbProps & {
+    render?: (props: BreadcrumbProps) => React.ReactNode;
+  };
   avatar?: AvatarProps;
   tags?: React.ReactElement<TagProps> | React.ReactElement<TagProps>[];
   onBack?: (event?: React.MouseEvent<HTMLElement>) => void;
+
+  className?: string;
+  backIcon?: React.ReactNode;
+
+  render?: (
+    originNodes: {
+      title: React.ReactNode;
+      breadcrumb: React.ReactNode;
+    },
+    className?: string,
+  ) => React.ReactNode;
 };
 
 const renderTitle = (props: PageHeaderProps) => {
@@ -27,7 +41,7 @@ const renderTitle = (props: PageHeaderProps) => {
   // const backIcon = getBackIcon(props, direction);
   // const backIconDom = renderBack(backIcon, onBack);
   return (
-    <div className={cn("flex items-center justify-between")}>
+    <div className={cn("flex h-10 items-center justify-between")}>
       <div className="">
         {/* {backIconDom} */}
         {avatar && <Avatar className={cn(avatar.className)} {...avatar} />}
@@ -65,23 +79,24 @@ const renderChildren = (props: PageHeaderProps) => {
   return <div>{children}</div>;
 };
 
-const PageHeader = (props: PageHeaderProps) => {
-  const {
-    // style,
-    // footer,
-    // children,
-    // breadcrumb,
-    // breadcrumbRender,
-    className,
-    // contentWidth,
-    // layout,
-  } = props;
+const renderBreadcrumb = (breadcrumb?: PageHeaderProps["breadcrumb"]) => {
+  if (breadcrumb?.render) return breadcrumb.render(breadcrumb);
+  if (!breadcrumb?.items?.length) return <></>;
+  return <Breadcrumb {...breadcrumb} />;
+};
 
+const PageHeader = ({ className, render, ...props }: PageHeaderProps) => {
   const title = renderTitle(props);
   const children = renderChildren(props);
+  const breadcrumb = renderBreadcrumb(props.breadcrumb);
+
+  if (render) {
+    return render({ title, breadcrumb }, className);
+  }
 
   return (
-    <div className={cn(className)}>
+    <div className={cn("space-y-2", className)}>
+      {breadcrumb}
       {title}
       {children}
     </div>
