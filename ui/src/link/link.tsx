41aadb823f46c8ea4e1294d2664f8ed2ca2a0b28
VyDucTan <vdt5snet@gmail.com>
2025-01-09 11:54:32 +0700

    refactor: split link to nextjs or react router
    Introduce theSearch component to enhance search capabilities 
    within the application. This component utilizes Next.js routing and 
    debouncing to manage search queries efficiently. It updates the URL 
    with search parameters, allowing for a seamless user experience. 
    Additionally, configure ESLint for Next.js and React Router 
    frameworks to maintain code quality.

--- ui/src/link/link.tsx
+++ ui/src/link/link.tsx
@@ -1,29 +1,15 @@
-import type { LinkProps as NextLinkProps } from "next/link";
 import type { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
 import { forwardRef } from "react";
-import { NavLink } from "react-router";
 
-type LinkProps = NextLinkProps &
-  Omit<
-    DetailedHTMLProps<
-      AnchorHTMLAttributes<HTMLAnchorElement>,
-      HTMLAnchorElement
-    >,
-    "href"
-  >;
-const Link = forwardRef(
-  (
-    { href, prefetch, ...props }: LinkProps,
-    ref: React.Ref<HTMLAnchorElement>,
-  ) => {
-    return (
-      <NavLink
-        ref={ref}
-        to={typeof href === "string" ? href : (href.href ?? "")}
-        prefetch={prefetch ? "render" : undefined}
-        {...props}
-      />
-    );
-  },
-);
+type LinkProps = DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+> & {
+  prefetch?: boolean;
+};
+const Link = forwardRef((props: LinkProps) => {
+  return <a {...props} />;
+});
+
+export type { LinkProps };
 export { Link };
