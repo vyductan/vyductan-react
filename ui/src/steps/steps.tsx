780d42b43648d1ea36c561b1f36ee7617bbc33b8
VyDucTan <vdt5snet@gmail.com>
2025-01-10 18:55:57 +0700

    feat(steps): enhance step item rendering and icons
    Update the `Steps` component to improve item rendering by 
    introducing a customizable `itemRender` prop. Replace the 
    error icon with `CloseOutlined` for better visual clarity. 
    Refactor the component's structure for improved readability 
    and maintainability, ensuring a more flexible and user-friendly 
    interface.

--- ui/src/steps/steps.tsx
+++ ui/src/steps/steps.tsx
@@ -4,23 +4,44 @@ import type { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 import { useMergedState } from "rc-util";
 
 import { cn } from "..";
+import { CloseOutlined } from "../icons";
 import { CheckFilled } from "../icons/check-filled";
 
+type StepItemDef = {
+  title: ReactNode;
+  description?: ReactNode;
+  icon?: React.ReactNode;
+};
 type StepsProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 > & {
   current?: number;
   labelPlacement?: "vertical" | "horizontal";
-  items: { title: ReactNode; description?: ReactNode }[];
+  items: StepItemDef[];
+  itemRender?: (
+    step: StepItemDef,
+    index: number,
+    current: number,
+    icons: {
+      finish: React.ReactNode;
+      error: React.ReactNode;
+    },
+  ) => React.ReactNode;
+
+  classNames?: {
+    item?: string;
+  };
 };
 const Steps = (props: StepsProps) => {
   const {
     className,
+    classNames,
 
     current: currentProp = 0,
     labelPlacement = "horizontal",
     items,
+    itemRender,
   } = props;
   const [current] = useMergedState(0, {
     value: currentProp,
@@ -28,49 +49,54 @@ const Steps = (props: StepsProps) => {
 
   const icons = {
     finish: <CheckFilled />,
-    error: <CheckFilled />,
+    error: <CloseOutlined />,
   };
   return (
     <div className={cn("flex w-full", className)}>
       {items.map((x, index) => {
         return (
           <div
-            key={index}
             className={cn(
-              "relative flex",
-              index < items.length - 1 && "flex-1",
+              // "px-4 first:ps-0",
+              "flex-1 last:flex-none",
+              "outline-none",
+              // "relative inline-block pe-4",
+              "flex items-center",
+              labelPlacement === "vertical" && "flex-col",
+              current < index && "text-foreground-muted",
+              "after:mx-4 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
+              classNames?.item,
             )}
           >
-            <div
-              className={cn(
-                "flex w-20 items-center",
-                labelPlacement === "vertical" && "flex-col",
-                current < index && "text-foreground-muted",
-              )}
-            >
-              {index < items.length - 1 && (
+            {/* Line */}
+            {/* {index < items.length - 1 && (
                 <div
                   className={cn(
                     "absolute start-0 top-1 h-10 w-full px-6",
-                    "after:ms-10 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-[background-color_0.3s]",
+                    "after:ms-10 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
                     current > index && "after:bg-primary-600",
                   )}
                 />
-              )}
-              <div
-                className={cn(
-                  "flex size-8 items-center justify-center rounded-full transition-[background-color_0.3s,border-color_0.3s]",
-                  "bg-background-muted text-foreground-muted",
-                  current === index && "bg-primary-600 text-white",
-                  current > index && "bg-primary-200 text-primary-600",
-                )}
-              >
-                {current > index ? icons.finish : index}
-              </div>
-              <div className="mt-5 flex flex-col justify-center text-center">
-                {x.title}
-              </div>
-            </div>
+              )} */}
+            {itemRender ? (
+              itemRender(x, index, current, icons)
+            ) : (
+              <>
+                <div
+                  className={cn(
+                    "flex size-8 items-center justify-center rounded-full transition-colors duration-300",
+                    "bg-background-muted text-foreground-muted",
+                    current === index && "bg-primary-600 text-white",
+                    current > index && "bg-primary-200 text-primary-600",
+                  )}
+                >
+                  {current > index ? icons.finish : index}
+                </div>
+                <div className="mt-5 flex flex-col justify-center text-center">
+                  {x.title}
+                </div>
+              </>
+            )}
           </div>
         );
       })}
@@ -78,4 +104,5 @@ const Steps = (props: StepsProps) => {
   );
 };
 
+export type { StepItemDef, StepsProps };
 export { Steps };
