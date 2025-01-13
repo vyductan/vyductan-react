f881befc1df6ea8e4eba809f9fb6af536d5c8604
VyDucTan <vdt5snet@gmail.com>
2025-01-13 17:49:09 +0700

    feat(steps): add line class support and improve layout
    Adds a new `line` class to the `StepsProps` for better styling 
    options. Adjusts the layout to ensure items are centered and 
    improves the visual separation between steps by implementing 
    a line between them. This enhances the overall user experience 
    and visual clarity of the steps component.

--- ui/src/steps/steps.tsx
+++ ui/src/steps/steps.tsx
@@ -1,3 +1,5 @@
+// Line: https://mui.com/material-ui/react-stepper/?srsltid=AfmBOoqD9hPvI4qfGHM8EO7B2ukciwhocSiX6o_Q0CQjZcZVVOkYWGUL
+// TODO: description should below title and line inline with title https://ant.design/components/steps
 "use client";
 
 import type { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
@@ -31,6 +33,7 @@ type StepsProps = DetailedHTMLProps<
 
   classNames?: {
     item?: string;
+    line?: string;
   };
 };
 const Steps = (props: StepsProps) => {
@@ -52,52 +55,56 @@ const Steps = (props: StepsProps) => {
     error: <CloseOutlined />,
   };
   return (
-    <div className={cn("flex w-full", className)}>
+    <div className={cn("flex w-full items-center", className)}>
       {items.map((x, index) => {
         return (
-          <div
-            className={cn(
-              // "px-4 first:ps-0",
-              "flex-1 last:flex-none",
-              "outline-none",
-              // "relative inline-block pe-4",
-              "flex items-center",
-              labelPlacement === "vertical" && "flex-col",
-              current < index && "text-foreground-muted",
-              "after:mx-4 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
-              classNames?.item,
-            )}
-          >
+          <>
+            <div
+              className={cn(
+                // "px-4 first:ps-0",
+                "flex-1 last:flex-none",
+                "outline-none",
+                // "relative inline-block pe-4",
+                "flex items-center",
+                labelPlacement === "vertical" && "flex-col",
+                current < index && "text-foreground-muted",
+                // "after:mx-4 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
+                classNames?.item,
+              )}
+            >
+              {itemRender ? (
+                itemRender(x, index, current, icons)
+              ) : (
+                <>
+                  <div
+                    className={cn(
+                      "flex size-8 items-center justify-center rounded-full transition-colors duration-300",
+                      "bg-background-muted text-foreground-muted",
+                      current === index && "bg-primary-600 text-white",
+                      current > index && "bg-primary-200 text-primary-600",
+                    )}
+                  >
+                    {current > index ? icons.finish : index}
+                  </div>
+                  <div className="mt-5 flex flex-col justify-center text-center">
+                    {x.title}
+                  </div>
+                </>
+              )}
+            </div>
             {/* Line */}
-            {/* {index < items.length - 1 && (
-                <div
-                  className={cn(
-                    "absolute start-0 top-1 h-10 w-full px-6",
-                    "after:ms-10 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
-                    current > index && "after:bg-primary-600",
-                  )}
-                />
-              )} */}
-            {itemRender ? (
-              itemRender(x, index, current, icons)
-            ) : (
-              <>
-                <div
-                  className={cn(
-                    "flex size-8 items-center justify-center rounded-full transition-colors duration-300",
-                    "bg-background-muted text-foreground-muted",
-                    current === index && "bg-primary-600 text-white",
-                    current > index && "bg-primary-200 text-primary-600",
-                  )}
-                >
-                  {current > index ? icons.finish : index}
-                </div>
-                <div className="mt-5 flex flex-col justify-center text-center">
-                  {x.title}
-                </div>
-              </>
+            {index < items.length - 1 && (
+              <div
+                className={cn(
+                  // "absolute start-0 top-1 h-10 w-full px-6",
+                  // "after:ms-10 after:inline-block after:h-px after:w-full after:bg-background-muted after:transition-colors after:duration-300",
+                  // current > index && "after:bg-primary-600",
+                  "mx-4 inline-block h-px w-full bg-background-muted transition-colors duration-300",
+                  classNames?.line,
+                )}
+              />
             )}
-          </div>
+          </>
         );
       })}
     </div>
