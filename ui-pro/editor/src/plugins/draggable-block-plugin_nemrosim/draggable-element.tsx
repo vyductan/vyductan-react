3ee2469380a8100248970c55ba897660a52a4f3f
VyDucTan <vdt5snet@gmail.com>
2025-03-03 11:19:08 +0700

    merge ui

---
+++ ui-pro/editor/src/plugins/draggable-block-plugin_nemrosim/draggable-element.tsx
@@ -0,0 +1,48 @@
+import type { DragEvent as ReactDragEvent } from "react";
+import { memo, useCallback } from "react";
+
+import { cn } from "@acme/ui";
+
+import { useDraggableStore } from "../../stores/use-draggable-store";
+
+const DraggableElement = () => {
+  const { draggable, resetState } = useDraggableStore();
+
+  const handleOnDragStart = useCallback(
+    ({ dataTransfer }: ReactDragEvent<HTMLDivElement>) => {
+      if (!draggable?.htmlElement) {
+        return;
+      }
+      // THIS WILL SET THE DRAGGABLE IMAGE
+      dataTransfer.setDragImage(draggable.htmlElement, 0, 0);
+    },
+    [draggable?.htmlElement],
+  );
+
+  if (!draggable?.data) {
+    return <></>;
+  }
+
+  const scrollOffset = document.body.getBoundingClientRect().top;
+
+  return (
+    <div
+      style={{
+        top: draggable.data.top - scrollOffset,
+        left: draggable.data.left - 23,
+        height: draggable.data.height,
+      }}
+      className={cn(
+        "absolute h-10 w-4 cursor-grab bg-red-500 transition-[background]",
+        "active:cursor-grabbing",
+        "hover:bg-blue-600",
+      )}
+      onDragStart={handleOnDragStart}
+      onDragEnd={resetState}
+    />
+  );
+};
+
+const Memoized = memo(DraggableElement, () => true);
+
+export { Memoized as DraggableElement };
