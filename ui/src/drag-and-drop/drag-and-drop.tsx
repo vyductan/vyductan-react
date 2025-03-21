538720320822541a93e1edfa04279208a9359142
VyDucTan <vdt5snet@gmail.com>
2025-03-21 11:20:27 +0700

    refactor(dnd): move dnd component to new package

--- ui/src/drag-and-drop/drag-and-drop.tsx
+++
@@ -1,7 +0,0 @@
-import type { DndContextProps } from "@dnd-kit/core";
-import { DndContext } from "@dnd-kit/core";
-
-type DragAndDropProps = DndContextProps;
-export const DragAndDrop = ({ ...props }: DragAndDropProps) => {
-  return <DndContext {...props} />;
-};
--- ui/src/drag-and-drop/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./drag-and-drop";
-export * from "./droppable";
-export * from "./draggable";
-export * from "./sortable";
-export * from "./sortable-context";
