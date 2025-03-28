f36e3147320d215ef42a4bb62e8f6d66ab97a4dc
VyDucTan <vdt5snet@gmail.com>
2025-03-28 16:48:05 +0700

    fix(radio): remove lucide-icon

--- ui/src/radio/_components.tsx
+++ ui/src/radio/_components.tsx
@@ -5,6 +5,7 @@ import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import { CircleIcon } from "lucide-react";
 
 import { cn } from "..";
+import { Icon } from "../icons";
 
 function RadioGroupRoot({
   className,
@@ -36,7 +37,10 @@ function RadioGroupItem({
         data-slot="radio-group-indicator"
         className="relative flex items-center justify-center"
       >
-        <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
+        <Icon
+          icon="icon-[mdi--circle]"
+          className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2"
+        />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
