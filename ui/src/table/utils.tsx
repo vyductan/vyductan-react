e02335b6c1f3d4d3dc5c33d6cf34e45bd62eee22
VyDucTan <vdt5snet@gmail.com>
2025-04-15 17:48:22 +0700

    fix(table): missing selection and expandable before transform

--- ui/src/table/utils.tsx
+++ ui/src/table/utils.tsx
@@ -4,8 +4,6 @@ import type { ReactNode } from "react";
 import type { AnyObject } from "../types";
 import type { TableProps } from "./table";
 import type { ColumnDef, ColumnsDef } from "./types";
-import { Checkbox } from "../checkbox";
-import { Icon } from "../icons";
 
 export const transformColumnDefs = <TRecord extends AnyObject>(
   columns: ColumnsDef<TRecord>,
@@ -49,7 +47,8 @@ export const transformColumnDefs = <TRecord extends AnyObject>(
               id: key ?? index.toString(),
               accessorFn: () => key ?? index.toString(),
             }),
-        header: () => title,
+        header: ({ table }) =>
+          typeof title === "function" ? title({ table }) : title,
         ...(children
           ? {
               columns: transformColumnDefs(
@@ -110,7 +109,7 @@ export const transformColumnDefs = <TRecord extends AnyObject>(
           : { enableSorting: false }),
         ...restProps,
       };
-      columnDefMerged.cell = ({ column, row, getValue }) => (
+      columnDefMerged.cell = ({ column, row, getValue, table }) => (
         <>
           {isNotFirstDeepColumn && index === 0 && (
             <>
@@ -141,6 +140,7 @@ export const transformColumnDefs = <TRecord extends AnyObject>(
                   value: getValue() as never,
                   record: row.original,
                   index: row.index,
+                  table,
                   column,
                   row,
                 })
@@ -148,6 +148,7 @@ export const transformColumnDefs = <TRecord extends AnyObject>(
                   value: undefined as never,
                   record: row.original,
                   index: row.index,
+                  table,
                   column,
                   row,
                 })
@@ -159,105 +160,105 @@ export const transformColumnDefs = <TRecord extends AnyObject>(
     },
   );
 
-  if (props.rowSelection) {
-    let lastSelectedId = "";
+  // if (props.rowSelection) {
+  //   let lastSelectedId = "";
 
-    const selectionColumn: TTColumnDef<TRecord> = {
-      id: "selection",
-      header: ({ table }) => {
-        const originNode = (
-          <Checkbox
-            aria-label="Select all"
-            className="flex items-center justify-center"
-            checked={table.getIsAllPageRowsSelected()}
-            indeterminate={table.getIsSomePageRowsSelected()}
-            onChange={table.toggleAllPageRowsSelected}
-          />
-        );
-        return props.rowSelection?.renderHeader
-          ? props.rowSelection.renderHeader({
-              checked: table.getIsAllPageRowsSelected(),
-              originNode,
-            })
-          : originNode;
-      },
-      cell: ({ row, table }) => {
-        const originNode = (
-          <Checkbox
-            aria-label="Select row"
-            className="flex items-center justify-center"
-            checked={row.getIsSelected()}
-            disabled={!row.getCanSelect()}
-            onChange={row.getToggleSelectedHandler()}
-            onClick={(event) => {
-              if (event.shiftKey) {
-                const { rows, rowsById } = table.getRowModel();
-                const rowsToToggle = getRowRange(rows, row.id, lastSelectedId);
-                const isLastSelected =
-                  rowsById[lastSelectedId]?.getIsSelected();
-                for (const row of rowsToToggle)
-                  row.toggleSelected(isLastSelected);
-              }
+  //   const selectionColumn: TTColumnDef<TRecord> = {
+  //     id: "selection",
+  //     header: ({ table }) => {
+  //       const originNode = (
+  //         <Checkbox
+  //           aria-label="Select all"
+  //           className="flex items-center justify-center"
+  //           checked={table.getIsAllPageRowsSelected()}
+  //           indeterminate={table.getIsSomePageRowsSelected()}
+  //           onChange={table.toggleAllPageRowsSelected}
+  //         />
+  //       );
+  //       return props.rowSelection?.renderHeader
+  //         ? props.rowSelection.renderHeader({
+  //             checked: table.getIsAllPageRowsSelected(),
+  //             originNode,
+  //           })
+  //         : originNode;
+  //     },
+  //     cell: ({ row, table }) => {
+  //       const originNode = (
+  //         <Checkbox
+  //           aria-label="Select row"
+  //           className="flex items-center justify-center"
+  //           checked={row.getIsSelected()}
+  //           disabled={!row.getCanSelect()}
+  //           onChange={row.getToggleSelectedHandler()}
+  //           onClick={(event) => {
+  //             if (event.shiftKey) {
+  //               const { rows, rowsById } = table.getRowModel();
+  //               const rowsToToggle = getRowRange(rows, row.id, lastSelectedId);
+  //               const isLastSelected =
+  //                 rowsById[lastSelectedId]?.getIsSelected();
+  //               for (const row of rowsToToggle)
+  //                 row.toggleSelected(isLastSelected);
+  //             }
 
-              lastSelectedId = row.id;
-            }}
-          />
-        );
-        return props.rowSelection?.renderCell
-          ? props.rowSelection.renderCell({
-              checked: row.getIsSelected(),
-              record: row.original,
-              index: row.index,
-              originNode,
-            })
-          : originNode;
-      },
-      size: 32,
-      minSize: 32,
-      meta: {
-        align: "center",
-      },
-      enableSorting: false,
-      enableHiding: false,
-    };
-    columnsDef.unshift(selectionColumn);
-  }
+  //             lastSelectedId = row.id;
+  //           }}
+  //         />
+  //       );
+  //       return props.rowSelection?.renderCell
+  //         ? props.rowSelection.renderCell({
+  //             checked: row.getIsSelected(),
+  //             record: row.original,
+  //             index: row.index,
+  //             originNode,
+  //           })
+  //         : originNode;
+  //     },
+  //     size: 32,
+  //     minSize: 32,
+  //     meta: {
+  //       align: "center",
+  //     },
+  //     enableSorting: false,
+  //     enableHiding: false,
+  //   };
+  //   columnsDef.unshift(selectionColumn);
+  // }
 
-  if (props.expandable) {
-    const expandColumn: TTColumnDef<TRecord> = {
-      id: "expander",
-      size: 50,
-      meta: {
-        align: "center",
-      },
-      cell: ({ row }) => {
-        return row.getCanExpand() ? (
-          <button
-            {...{
-              onClick: () => {
-                if (!props.expandable?.expandRowByClick) {
-                  row.getToggleExpandedHandler()();
-                }
-              },
-            }}
-            className="flex w-full cursor-pointer items-center justify-center"
-          >
-            {row.getIsExpanded() ? (
-              <Icon icon="icon-[lucide--chevron-down]" />
-            ) : (
-              <Icon icon="icon-[lucide--chevron-right]" />
-            )}
-          </button>
-        ) : undefined;
-      },
-    };
-    columnsDef.unshift(expandColumn);
-  }
+  // if (props.expandable) {
+  //   const expandColumn: TTColumnDef<TRecord> = {
+  //     id: "expander",
+  //     size: 50,
+  //     meta: {
+  //       align: "center",
+  //     },
+  //     cell: ({ row }) => {
+  //       return row.getCanExpand() ? (
+  //         <button
+  //           {...{
+  //             onClick: () => {
+  //               if (!props.expandable?.expandRowByClick) {
+  //                 row.getToggleExpandedHandler()();
+  //               }
+  //             },
+  //           }}
+  //           className="flex w-full cursor-pointer items-center justify-center"
+  //         >
+  //           {row.getIsExpanded() ? (
+  //             <Icon icon="icon-[lucide--chevron-down]" />
+  //           ) : (
+  //             <Icon icon="icon-[lucide--chevron-right]" />
+  //           )}
+  //         </button>
+  //       ) : undefined;
+  //     },
+  //   };
+  //   columnsDef.unshift(expandColumn);
+  // }
 
   return columnsDef;
 };
 
-function getRowRange<T>(rows: Array<Row<T>>, idA: string, idB: string) {
+export function getRowRange<T>(rows: Array<Row<T>>, idA: string, idB: string) {
   const range: Array<Row<T>> = [];
   let foundStart = false;
   let foundEnd = false;
