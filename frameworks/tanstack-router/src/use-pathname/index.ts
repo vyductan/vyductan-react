b80222067bdf564b10c239250d4b3c19f590b8bb
VyDucTan <vdt5snet@gmail.com>
2025-04-16 09:41:01 +0700

    refactor(frameworks)!: remove tanstack-router

--- frameworks/tanstack-router/src/use-pathname/index.ts
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "@tanstack/react-router";
-
-export const usePathname = () => {
-  const pathname = useLocation({
-    select: (location) => location.pathname,
-  });
-  return pathname;
-};
