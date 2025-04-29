e04d79e9c7e06e0e0a5015e83f4b31f7bf94e83c
VyDucTan <vdt5snet@gmail.com>
2025-04-29 18:01:51 +0700

    fix(textarea): borderless variant

--- ui/src/components/textarea/textarea.tsx
+++ ui/src/components/textarea/textarea.tsx
@@ -10,7 +10,8 @@ import { inputVariants } from "../input";
 
 const textareaVariants = cva(
   [
-    "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+    "placeholder:text-muted-foreground field-sizing-content min-h-16 disabled:cursor-not-allowed disabled:opacity-50 ",
+    // "border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] md:text-sm",
     // own
     "text-sm",
   ],
@@ -44,12 +45,12 @@ export type TextareaProps = Omit<
   };
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ autoSize, borderless, className, size, status, ...props }, ref) => {
+  ({ autoSize, className, size, status, variant, ...props }, ref) => {
     const Comp = autoSize ? TextareaAutosize : "textarea";
     return (
       <Comp
         className={cn(
-          inputVariants({ borderless, status }),
+          inputVariants({ variant, status }),
           textareaVariants({ className, size }),
         )}
         ref={ref}
