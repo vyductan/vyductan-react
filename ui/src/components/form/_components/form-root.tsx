209063d87d391b9580db7ee8eb429e472e8d4474
VyDucTan <vdt5snet@gmail.com>
2025-05-06 11:02:29 +0700

    refactor(form): support shadcn base

---
+++ ui/src/components/form/_components/form-root.tsx
@@ -0,0 +1,34 @@
+import type { FieldValues } from "react-hook-form";
+import { FormProvider as RHFormProvider } from "react-hook-form";
+
+import type { FormProviderProps } from "../context";
+import { FormContext } from "../context";
+import { FormErrorsNotification } from "./form-errors-notification";
+
+// type FormProviderProps=
+const FormProvider = <
+  TFieldValues extends FieldValues,
+  TContext = any,
+  TTransformedValues extends FieldValues | undefined = undefined,
+>({
+  children,
+  ...props
+}: FormProviderProps<TFieldValues, TContext, TTransformedValues>) => {
+  return (
+    <FormContext.Provider
+      value={props as unknown as FormProviderProps<FieldValues, any, undefined>}
+    >
+      <RHFormProvider {...props}>{children}</RHFormProvider>
+      <FormErrorsNotification />
+    </FormContext.Provider>
+  );
+};
+
+const FormRoot = FormProvider;
+
+export { FormRoot };
+
+export {
+  type FormProviderProps,
+  type FormProviderProps as FormRootProps,
+} from "../context";
