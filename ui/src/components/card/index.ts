73fd0159baff0c5ad9dca0617ef3df4d12592bd1
VyDucTan <vdt5snet@gmail.com>
2025-05-16 17:13:05 +0700

    refactor(card): compatible with shadcn

--- ui/src/components/card/index.ts
+++ ui/src/components/card/index.ts
@@ -1,11 +1,2 @@
 export * from "./card";
-
-export {
-  CardRoot,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@acme/ui/shadcn/card";
+export * from "./_component";
