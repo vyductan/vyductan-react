217d43107d1a6589c729822ea7f6afd0a15b97ef
VyDucTan <vdt5snet@gmail.com>
2025-05-19 13:52:42 +0700

    feat(date-picker): compounded component

--- ui/src/components/date-picker/index.ts
+++ ui/src/components/date-picker/index.ts
@@ -1,4 +1,22 @@
+import { DatePicker as InternalDatePicker } from "./date-picker";
+import { DateRangePicker } from "./date-range-picker";
+import { MonthPicker } from "./month-picker";
+import { YearPicker } from "./year-picker";
+
 export * from "./date-picker";
 export * from "./date-range-picker";
 export * from "./month-picker";
 export * from "./year-picker";
+
+type CompoundedComponent = typeof InternalDatePicker & {
+  RangePicker: typeof DateRangePicker;
+  MonthPicker: typeof MonthPicker;
+  YearPicker: typeof YearPicker;
+};
+
+const DatePicker = InternalDatePicker as CompoundedComponent;
+DatePicker.RangePicker = DateRangePicker;
+DatePicker.MonthPicker = MonthPicker;
+DatePicker.YearPicker = YearPicker;
+
+export { DatePicker };
