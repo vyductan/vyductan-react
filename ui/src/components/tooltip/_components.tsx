21ca64217548df6f65962951d6c84826c5f05196
VyDucTan <vdt5snet@gmail.com>
2025-05-19 11:14:18 +0700

    refactor(tooltip): export some components from shadcn

--- ui/src/components/tooltip/_components.tsx
+++ ui/src/components/tooltip/_components.tsx
@@ -1,38 +1,6 @@
-"use client";
-
-import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 import { cn } from "@acme/ui/lib/utils";
-
-function TooltipProvider({
-  delayDuration = 0,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
-  return (
-    <TooltipPrimitive.Provider
-      data-slot="tooltip-provider"
-      delayDuration={delayDuration}
-      {...props}
-    />
-  );
-}
-
-function TooltipRoot({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
-  return (
-    <TooltipProvider>
-      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
-    </TooltipProvider>
-  );
-}
-
-function TooltipTrigger({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />;
-}
 
 function TooltipContent({
   className,
@@ -68,4 +36,10 @@ function TooltipContent({
   );
 }
 
-export { TooltipRoot, TooltipTrigger, TooltipContent, TooltipProvider };
+export {
+  TooltipProvider,
+  Tooltip as TooltipRoot,
+  TooltipTrigger,
+} from "@acme/ui/shadcn/tooltip";
+
+export { TooltipContent };
