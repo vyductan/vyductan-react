79f10b40b1445baf95673bcc923a36e7e83f415b
VyDucTan <vdt5snet@gmail.com>
2025-05-19 13:54:15 +0700

    feat(input): compounded components

--- ui/src/components/input/index.ts
+++ ui/src/components/input/index.ts
@@ -1,4 +1,22 @@
+import { Textarea } from "../textarea";
+import { Input as InternalInput } from "./input";
+import { InputNumber } from "./number";
+import { InputPassword } from "./password";
+
 export * from "./input";
 export * from "./number";
 export * from "./password";
 export * from "./types";
+
+type CompoundedComponent = typeof InternalInput & {
+  TextArea: typeof Textarea;
+  Number: typeof InputNumber;
+  Password: typeof InputPassword;
+};
+
+const Input = InternalInput as CompoundedComponent;
+Input.TextArea = Textarea;
+Input.Number = InputNumber;
+Input.Password = InputPassword;
+
+export { Input };
