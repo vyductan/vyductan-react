89a54669f7e17b2781341bb7b2ee418485a86436
VyDucTan <vdt5snet@gmail.com>
2025-05-29 17:49:29 +0700

    refactor(sheet): compatible with shadcn

--- ui/src/components/sheet/sheet.tsx
+++ ui/src/components/sheet/sheet.tsx
@@ -1,16 +1,20 @@
+import type { XOR } from "ts-xor";
+
 import { cn } from "@acme/ui/lib/utils";
 
-import type { SheetContentProps, SheetRootProps } from "../../shadcn/sheet";
 import {
   SheetContent,
   SheetDescription,
   SheetHeader,
-  SheetRoot,
+  Sheet as SheetRoot,
   SheetTitle,
   SheetTrigger,
 } from "../../shadcn/sheet";
 
-type DrawerProps = SheetRootProps & {
+type ShadcnSheetProps = React.ComponentProps<typeof SheetRoot> & {
+  title?: React.ReactNode;
+};
+type OwnSheetProps = {
   title: React.ReactNode;
   description?: React.ReactNode;
   children?: React.ReactNode;
@@ -22,24 +26,31 @@ type DrawerProps = SheetRootProps & {
     description?: string;
     content?: string;
   };
-  placement?: SheetContentProps["side"];
-  closeIcon?: SheetContentProps["closeIcon"];
+  placement?: React.ComponentProps<typeof SheetContent>["side"];
 };
-const Drawer = ({
-  title,
-  description,
-  children,
-  trigger,
 
-  classNames,
-  placement = "right",
-  closeIcon,
-  ...props
-}: DrawerProps) => {
+type SheetProps = XOR<ShadcnSheetProps, OwnSheetProps>;
+const Sheet = (props: SheetProps) => {
+  const isShadcnSheet = !props.title;
+  if (isShadcnSheet) {
+    return <SheetRoot {...props} />;
+  }
+
+  const {
+    title,
+    description,
+    children,
+    trigger,
+
+    classNames,
+    placement = "right",
+    ...restProps
+  } = props;
+
   return (
-    <SheetRoot {...props}>
+    <SheetRoot {...restProps}>
       {trigger && <SheetTrigger asChild>{trigger}</SheetTrigger>}
-      <SheetContent side={placement} closeIcon={closeIcon}>
+      <SheetContent side={placement}>
         <SheetHeader className={cn("p-6 pb-0", classNames?.header)}>
           <SheetTitle className={classNames?.title}>{title}</SheetTitle>
           <SheetDescription
@@ -55,5 +66,15 @@ const Drawer = ({
   );
 };
 
-export type { DrawerProps };
-export { Drawer };
+export { Sheet };
+
+export {
+  Sheet as SheetRoot,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from "../../shadcn/sheet";
