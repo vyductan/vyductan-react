7edee29efd28c5b750eee0574845b4c72d7f0abb
VyDucTan <vdt5snet@gmail.com>
2025-06-11 14:16:00 +0700

    refactor(popover): remove unused code in shadcn

--- ui/src/shadcn/popover.tsx
+++ ui/src/shadcn/popover.tsx
@@ -3,8 +3,9 @@ import * as PopoverPrimitive from "@radix-ui/react-popover";
 
 import { cn } from "@acme/ui/lib/utils";
 
-type PopoverRootProps = React.ComponentProps<typeof PopoverPrimitive.Root>;
-function PopoverRoot({ ...props }: PopoverRootProps) {
+function Popover({
+  ...props
+}: React.ComponentProps<typeof PopoverPrimitive.Root>) {
   return <PopoverPrimitive.Root data-slot="popover" {...props} />;
 }
 
@@ -14,15 +15,12 @@ function PopoverTrigger({
   return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />;
 }
 
-type PopoverContentProps = React.ComponentProps<
-  typeof PopoverPrimitive.Content
->;
 function PopoverContent({
   className,
   align = "center",
   sideOffset = 4,
   ...props
-}: PopoverContentProps) {
+}: React.ComponentProps<typeof PopoverPrimitive.Content>) {
   return (
     <PopoverPrimitive.Portal>
       <PopoverPrimitive.Content
@@ -33,15 +31,6 @@ function PopoverContent({
           "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden",
           className,
         )}
-        //   style={{
-        //   boxShadow: `0 6px 16px 0 rgba(0, 0, 0, 0.08),
-        // 0 3px 6px -4px rgba(0, 0, 0, 0.12),
-        // 0 9px 28px 8px rgba(0, 0, 0, 0.05)`,
-        // }}
-        // to allow scrollable
-        onWheel={(event) => {
-          event.stopPropagation();
-        }}
         {...props}
       />
     </PopoverPrimitive.Portal>
@@ -54,11 +43,4 @@ function PopoverAnchor({
   return <PopoverPrimitive.Anchor data-slot="popover-anchor" {...props} />;
 }
 
-export type { PopoverRootProps, PopoverContentProps };
-export {
-  PopoverRoot as Popover,
-  PopoverRoot,
-  PopoverTrigger,
-  PopoverAnchor,
-  PopoverContent,
-};
+export { Popover, PopoverTrigger, PopoverAnchor, PopoverContent };
