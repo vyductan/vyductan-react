e05a755754ea02a1e68cc2971e073754e6bc5d19
VyDucTan <vdt5snet@gmail.com>
2025-06-25 11:27:16 +0700

    fix(popover): more clearly box-shadow

--- ui/src/components/popover/_component.tsx
+++ ui/src/components/popover/_component.tsx
@@ -24,11 +24,9 @@ const PopoverContent = ({
         e.preventDefault();
         onWheel?.(e);
       }}
-      //   style={{
-      //   boxShadow: `0 6px 16px 0 rgba(0, 0, 0, 0.08),
-      // 0 3px 6px -4px rgba(0, 0, 0, 0.12),
-      // 0 9px 28px 8px rgba(0, 0, 0, 0.05)`,
-      // }}
+      style={{
+        boxShadow: `var(--box-shadow-secondary)`,
+      }}
       {...props}
     />
   );
