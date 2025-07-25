1393dba807c34e6ec36be179ed1d1af6a4b1dc77
VyDucTan <vdt5snet@gmail.com>
2025-07-25 09:48:48 +0700

    feat(modal): confirm function

--- ui/src/components/modal/index.tsx
+++ ui/src/components/modal/index.tsx
@@ -12,6 +12,19 @@ export * from "./use-modal";
 
 type ShadcnModalProps = React.ComponentProps<typeof Dialog>;
 type XORModalProps = XOR<ModalProps, ShadcnModalProps>;
+
+// Confirm modal configuration interface (for reference)
+interface ConfirmConfig {
+  title?: React.ReactNode;
+  content?: React.ReactNode;
+  onOk?: () => void | Promise<void>;
+  onCancel?: () => void;
+  okText?: string;
+  cancelText?: string;
+  okType?: "default" | "primary" | "danger";
+  confirmLoading?: boolean;
+}
+
 const Modal = (props: XORModalProps) => {
   const isShadcnDialog = React.Children.toArray(props.children).some(
     (child) => React.isValidElement(child) && child.type === DialogContent,
@@ -21,6 +34,24 @@ const Modal = (props: XORModalProps) => {
   }
   return (
     <InternalModal {...(props as ModalProps)}>{props.children}</InternalModal>
+  );
+};
+
+// Confirm function following Ant Design API
+// Note: This is a static method that requires App component to be used
+Modal.confirm = (_config: ConfirmConfig) => {
+  throw new Error(
+    "Modal.confirm() can only be used within App component. " +
+      "Please wrap your app with <App> component and use useModal() hook instead.\n\n" +
+      "Example:\n" +
+      'import { App, useModal } from "@acme/ui";\n\n' +
+      "// Wrap your app\n" +
+      "<App>\n" +
+      "  <YourApp />\n" +
+      "</App>\n\n" +
+      "// In your component\n" +
+      "const modal = useModal();\n" +
+      "modal.confirm({ ... });",
   );
 };
 
