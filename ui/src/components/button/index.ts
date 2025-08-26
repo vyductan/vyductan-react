5ee415348343b97365e7b8e93ef596625a6e1ed2
VyDucTan <vdt5snet@gmail.com>
2025-08-26 14:46:11 +0700

    feat(button): export button variants type

--- ui/src/components/button/index.ts
+++ ui/src/components/button/index.ts
@@ -1,4 +1,5 @@
 export type { ButtonProps } from "./button";
+export type { ButtonVariants } from "./button";
 
 export { Button, buttonVariants } from "./button";
 export { LoadingIcon } from "./loading-icon";
