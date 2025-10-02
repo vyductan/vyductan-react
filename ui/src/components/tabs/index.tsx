c89167fb5aa4c4f7610587b157ea45e5516b3d7d
VyDucTan <vdt5snet@gmail.com>
2025-10-02 20:04:13 +0700

    feat(tabs): implement conditional tabs component with shadcn support
    introduce a new Tabs component that conditionally renders either the custom Tabs implementation or the shadcn TabsRoot based on the children passed, allowing for flexible usage while maintaining backward compatibility

--- ui/src/components/tabs/index.tsx
+++ ui/src/components/tabs/index.tsx
@@ -1,2 +1,25 @@
-export * from "./tabs";
+import type { XOR } from "ts-xor";
+import React, { Children, isValidElement } from "react";
+
+import type { TabsProps } from "./tabs";
+import { TabsContent, TabsList, TabsRoot } from "./_components";
+import { Tabs as OwnTabs } from "./tabs";
+
 export * from "./_components";
+
+type ShadcnTabsProps = React.ComponentProps<typeof TabsRoot>;
+type ConditionTabsProps = XOR<TabsProps, ShadcnTabsProps>;
+const Tabs = ({ children, ...props }: ConditionTabsProps) => {
+  const isShadcnTabs = Children.toArray(children).some(
+    (child) =>
+      isValidElement(child) &&
+      (child.type === TabsList || child.type === TabsContent),
+  );
+  if (isShadcnTabs) {
+    return <TabsRoot {...(props as ShadcnTabsProps)}>{children}</TabsRoot>;
+  }
+
+  return <OwnTabs {...(props as TabsProps)} />;
+};
+
+export { Tabs };
