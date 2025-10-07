8e4061c6f24f8bbb7c5fc699167193433d7bf0ab
VyDucTan <vdt5snet@gmail.com>
2025-10-07 22:40:48 +0700

    feat(radio): add type property to radio change event target

--- ui/src/components/radio/types.ts
+++ ui/src/components/radio/types.ts
@@ -2,8 +2,10 @@ import type { RadioProps } from "./radio";
 
 export interface RadioChangeEventTarget extends RadioProps {
   checked: boolean;
+  type: "checkbox" | "radio";
 }
 export interface RadioChangeEvent {
+  type: "change";
   target: RadioChangeEventTarget;
   stopPropagation: () => void;
   preventDefault: () => void;
