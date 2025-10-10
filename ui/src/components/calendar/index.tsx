73eadf466ddf914c04a9f17b63370bb2d5402541
VyDucTan <vdt5snet@gmail.com>
2025-10-10 15:56:58 +0700

    feat(calendar): implement custom calendar day button and integrate with calendar component
    - Added a new `CustomCalendarDayButton` component to enhance date selection with range highlighting.
    - Integrated the custom button into the `ShadcnCalendar` component for both single and multiple selection modes.
    - Updated the `mergedClassNames` utility to support custom styling for range dates.
    
    These changes improve the calendar's usability by providing visual feedback for date ranges and enhancing the overall user experience.

--- ui/src/components/calendar/index.tsx
+++ ui/src/components/calendar/index.tsx
@@ -12,6 +12,19 @@ import type { XOR } from "ts-xor";
 import type { CalendarProps as OwnCalendarProps } from "./calendar";
 import { Calendar as ShadcnCalendar } from "../../shadcn/calendar";
 import { Calendar as CalendarComponent } from "./calendar";
+import { CustomCalendarDayButton } from "./_components";
+import { cn } from "@/lib/utils";
+
+export const mergedClassNames = (
+  classNames: ShadcnCalendarProps["classNames"],
+): ShadcnCalendarProps["classNames"] => ({
+  day: cn(
+    "data-[range-middle=true]:bg-primary/20 data-[range-middle=true]:text-primary",
+    classNames?.day,
+  ),
+  range_start: cn("bg-transparent", classNames?.range_start),
+  range_end: cn("bg-transparent", classNames?.range_end),
+});
 
 type ShadcnCalendarProps = PropsBase &
   (
@@ -25,19 +38,33 @@ type ShadcnCalendarProps = PropsBase &
 
 type ConditionalProps = XOR<OwnCalendarProps, ShadcnCalendarProps>;
 
-const Calendar = (props: ConditionalProps) => {
+const Calendar = ({ classNames, ...props }: ConditionalProps) => {
   // if(props.mode === "single"){
   //     props.onSelect
   // }
+
   if ("selected" in props) {
-    return <ShadcnCalendar {...(props as ShadcnCalendarProps)} />;
+    return (
+      <ShadcnCalendar
+        classNames={mergedClassNames(classNames)}
+        components={{
+          DayButton: CustomCalendarDayButton,
+        }}
+        {...(props as ShadcnCalendarProps)}
+      />
+    );
   }
 
   //   if("value" in props) {
   //     return <CalendarComponent {...props.onSel} />;
   //   }
 
-  return <CalendarComponent {...(props as OwnCalendarProps)} />;
+  return (
+    <CalendarComponent
+      classNames={mergedClassNames(classNames)}
+      {...(props as OwnCalendarProps)}
+    />
+  );
 };
 
 export { Calendar };
