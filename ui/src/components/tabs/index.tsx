a023cccc2be0a08af52661fe944f7d2df6faa33a
VyDucTan <vdt5snet@gmail.com>
2025-10-14 17:32:10 +0700

    feat(tabs): enhance Tabs component to support conditional rendering based on props
    - Updated the Tabs component to determine if it should render as ShadcnTabs based on the presence of items in props, improving flexibility in usage.
    - This change allows for better integration with different tab configurations, enhancing the overall usability of the Tabs component within the UI toolkit.

--- ui/src/components/tabs/index.tsx
+++ ui/src/components/tabs/index.tsx
@@ -10,11 +10,12 @@ export * from "./_components";
 type ShadcnTabsProps = React.ComponentProps<typeof TabsRoot>;
 type ConditionTabsProps = XOR<TabsProps, ShadcnTabsProps>;
 const Tabs = ({ children, ...props }: ConditionTabsProps) => {
-  const isShadcnTabs = Children.toArray(children).some(
-    (child) =>
-      isValidElement(child) &&
-      (child.type === TabsList || child.type === TabsContent),
-  );
+  const isShadcnTabs =
+    Children.toArray(children).some(
+      (child) =>
+        isValidElement(child) &&
+        (child.type === TabsList || child.type === TabsContent),
+    ) || !props.items;
   if (isShadcnTabs) {
     return <TabsRoot {...(props as ShadcnTabsProps)}>{children}</TabsRoot>;
   }
