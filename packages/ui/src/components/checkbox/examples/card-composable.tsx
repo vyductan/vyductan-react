02c946df6ce0736e68050e758d0f1544f3bef36d
VyDucTan <vdt5snet@gmail.com>
2026-04-14 20:45:03 +0700

    ui: fix checkbox examples alignment

--- packages/ui/src/components/checkbox/examples/card-composable.tsx
+++ packages/ui/src/components/checkbox/examples/card-composable.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
           defaultChecked
           className="data-[state=checked]:border-primary-600 data-[state=checked]:bg-primary-600 dark:data-[state=checked]:border-primary-700 dark:data-[state=checked]:bg-primary-700 data-[state=checked]:text-white"
         />
-        <div className="grid gap-1.5 font-normal">
+        <div className="mt-px grid gap-1.5 font-normal">
           <p className="text-sm leading-none font-medium">Auto Start</p>
           <p className="text-muted-foreground text-sm">
             Starting with your OS.
@@ -20,7 +20,7 @@ const App: React.FC = () => {
       </Label>
       <Label className="hover:bg-accent/50 has-aria-checked:border-primary-600 has-aria-checked:bg-primary-50 dark:has-aria-checked:border-primary-900 dark:has-aria-checked:bg-primary-950 flex items-start gap-2 rounded-lg border p-3">
         <Checkbox className="data-[state=checked]:border-primary-600 data-[state=checked]:bg-primary-600 dark:data-[state=checked]:border-primary-700 dark:data-[state=checked]:bg-primary-700 data-[state=checked]:text-white" />
-        <div className="grid gap-1.5 font-normal">
+        <div className="mt-px grid gap-1.5 font-normal">
           <p className="text-sm leading-none font-medium">Auto update</p>
           <p className="text-muted-foreground text-sm">
             Download and install new version
--- packages/ui/src/components/checkbox/examples/card.tsx
+++ packages/ui/src/components/checkbox/examples/card.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="flex w-full max-w-sm flex-col gap-6">
       <Checkbox defaultChecked variant="card">
-        <div className="mt-1 grid gap-1.5 font-normal">
+        <div className="grid gap-1.5 font-normal">
           <p className="text-sm leading-none font-medium">Auto Start</p>
           <p className="text-muted-foreground text-sm">
             Starting with your OS.
